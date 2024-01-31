8cf7d5eba8cc77d744434ef86a0a16540f568a62
emiequine <emi.roberti@equineregister.co.uk>
2024-01-31 15:01:15 +0000

    improved user story navigation

--- components/userStory/UserStory.tsx
+++ components/userStory/UserStory.tsx
@@ -1,15 +1,22 @@
 import React, {FC} from 'react';
-import {View, Image, Text} from 'react-native';
+import {View, Image, Text, TouchableOpacity, Alert} from 'react-native';
 import style from './style';
 interface UserStoryProps {
   firstName: string;
   profileImage: any;
 }
 const UserStory: FC<UserStoryProps> = ({firstName, profileImage}) => {
+  const handlePress = () => {
+    Alert.alert(firstName);
+  };
   return (
     <View style={style.storyContainer}>
-      <Image source={profileImage} />
-      <Text style={style.firstName}>{firstName}</Text>
+      <TouchableOpacity onPress={handlePress}>
+        <View style={style.borderImageContainer}>
+          <Image style={style.image} source={profileImage} />
+        </View>
+        <Text style={style.firstName}>{firstName}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
--- components/userStory/style.ts
+++ components/userStory/style.ts
@@ -1,8 +1,25 @@
 import {StyleSheet} from 'react-native';
+import {getFontFamily} from '../../assets/fonts/helper';
 
 const style = StyleSheet.create({
   storyContainer: {marginRight: 20},
-  firstName: {},
+  firstName: {
+    fontFamily: getFontFamily('Inter', '500'),
+    fontSize: 14,
+    color: '#022150',
+    marginTop: 8,
+    textAlign: 'center',
+  },
+  borderImageContainer: {
+    borderColor: '#F35BAC',
+    borderWidth: 1,
+    padding: 3,
+    borderRadius: 50,
+  },
+  image: {
+    width: 55,
+    height: 55,
+  },
 });
 
 export default style;
